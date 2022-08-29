1e7848723c4f069915da7ad61d798a0272e39e25
Jared Siemen <siemenjm@gmail.com>
2022-08-29 19:30:24 -0400

    Add login post route to log user in to site and create session

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -20,11 +20,12 @@ router.get('/login', (req, res) => {
 });
 
 // POST ROUTES
+// create new user
 router.post('/register', async (req, res) => {
     try {
         const foundUser = await db.User.exists({ email: req.body.email});
         if (foundUser) {
-            res.redirect('/login');
+            return res.redirect('/login');
         }
 
         const salt = await bcrypt.genSalt(12);
@@ -33,6 +34,30 @@ router.post('/register', async (req, res) => {
 
         const newUser = await db.User.create(req.body);
         res.redirect('/login');
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+// login user
+router.post('/login', async (req, res) => {
+    try {
+        const foundUser = await db.User.findOne({ email: req.body.email });
+        if (!foundUser) {
+            return res.redirect('/register');
+        }
+
+        const match = await bcrypt.compare(req.body.password, foundUser.password);
+        if (!match) {
+            return res.send('password invalid')
+        }
+
+        req.session.currentUser = {
+            id: foundUser._id,
+            username: foundUser.username
+        };
+
+        return res.redirect('/posts');
     } catch(err) {
         console.log(err);
     }
