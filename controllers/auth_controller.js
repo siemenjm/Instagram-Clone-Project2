2e0d4bf624195e7a797eb5b3d5111e6b03455c81
Jared Siemen <siemenjm@gmail.com>
2022-08-29 16:53:36 -0400

    Add register post route to create new user

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -10,14 +10,33 @@ router.use(express.urlencoded({ extended: false }));
 
 // GET ROUTES
 // register page
-router.get("/register", (req, res) => {
+router.get('/register', (req, res) => {
   res.render('auth/register');
 });
 
 // login page
-router.get("/login", (req, res) => {
+router.get('/login', (req, res) => {
     res.render('auth/login');
-  });
+});
+
+// POST ROUTES
+router.post('/register', async (req, res) => {
+    try {
+        const foundUser = await db.User.exists({ email: req.body.email});
+        if (foundUser) {
+            res.redirect('/login');
+        }
+
+        const salt = await bcrypt.genSalt(12);
+        const hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
+
+        const newUser = await db.User.create(req.body);
+        res.redirect('/login');
+    } catch(err) {
+        console.log(err);
+    }
+});
 
 // EXPORT ROUTER
 module.exports = router;
