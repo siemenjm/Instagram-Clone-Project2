3f68d3ecce583f963184c754bb23e50843cb879a
Jared Siemen <siemenjm@gmail.com>
2022-08-31 10:50:41 -0400

    Automatically populate database with comments

--- models/seed_comments.js
+++ models/seed_comments.js
@@ -3,28 +3,48 @@ const db = require('./index');
 const seedComments = [
     {
         content: 'test content for comment 1',
-        user: "630e0fc81206dd62cd1e45cd", // need to update if users have been reloaded
-        post: "630e108786530defc7bfce1c" // need to update if posts have been reloaded
     },
     {
         content: 'test content for comment 2',
-        user: "630e0fc81206dd62cd1e45ce", // need to update if users have been reloaded
-        post: "630e108786530defc7bfce1d" // need to update if posts have been reloaded
     },
     {
         content: 'test content for comment 3',
-        user: "630e0fc81206dd62cd1e45cf", // need to update if users have been reloaded
-        post: "630e108786530defc7bfce1e" // need to update if posts have been reloaded
     }
 ];
+
+async function getUserIds() {
+    try {
+        const userIds = await db.User.find({}).select('_id');
+        
+        seedComments[0].user = userIds[0]._id.toString();
+        seedComments[1].user = userIds[1]._id.toString();
+        seedComments[2].user = userIds[2]._id.toString();
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+async function getPostIds() {
+    try {
+        const postIds = await db.Post.find({}).select('_id');
+        
+        seedComments[0].post = postIds[0]._id.toString();
+        seedComments[1].post = postIds[1]._id.toString();
+        seedComments[2].post = postIds[2]._id.toString();
+    } catch(err) {
+        console.log(err);
+    }
+}
 
 async function reloadComments() {
     try {
         const deletedComments = await db.Comment.deleteMany({});
-        console.log(deletedComments);
+        console.log('Deleted Comments: ', deletedComments);
 
+        await getUserIds();
+        await getPostIds();
         const reloadedComments = await db.Comment.insertMany(seedComments);
-        console.log(reloadedComments);
+        console.log('Reloaded Comments: ', reloadedComments);
     } catch(err) {
         console.log(err);
     }
